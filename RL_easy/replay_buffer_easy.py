589d4792832e75e6e662cf1c0286eecd3b53673e
matt60110552 <matt6011055.eed07@nctu.edu.tw>
2023-10-21 14:00:15 +0800

    add success data in buffer

--- RL_easy/replay_buffer_easy.py
+++ RL_easy/replay_buffer_easy.py
@@ -19,14 +19,14 @@ class ReplayBuffer(object):
 
         self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-    def add(self, pc_state, joint_state, con_action, conti_para, next_pc_state, next_joint_state, reward, done):
+    def add(self, pc_state, joint_state, con_action, conti_para, next_pc_state, next_joint_state, reward, done, success):
         if self.size < self.max_size:
-            self.buffer.put((pc_state, joint_state, con_action, conti_para, next_pc_state, next_joint_state, reward, done))
+            self.buffer.put((pc_state, joint_state, con_action, conti_para, next_pc_state, next_joint_state, reward, done, success))
             self.size += 1
         else:
             # Remove the oldest entry to make space for the new entry
             self.buffer.get()
-            self.buffer.put((pc_state, joint_state, con_action, conti_para, next_pc_state, next_joint_state, reward, done))
+            self.buffer.put((pc_state, joint_state, con_action, conti_para, next_pc_state, next_joint_state, reward, done, success))
 
     def sample(self, batch_size):
         batch = []
@@ -35,7 +35,7 @@ class ReplayBuffer(object):
         for i in random_idx:
             batch.append(self.buffer.queue[i])
         # self.buffer.queue.extend(batch)  # Re-extend the queue with the sampled data
-        pc_state_batch, joint_state_batch, con_action_batch, conti_para_batch, next_pc_state_batch, next_joint_state_batch, reward_batch, done_batch = zip(*batch)
+        pc_state_batch, joint_state_batch, con_action_batch, conti_para_batch, next_pc_state_batch, next_joint_state_batch, reward_batch, done_batch, success_batch = zip(*batch)
 
         return (
             np.stack(pc_state_batch),
@@ -45,14 +45,15 @@ class ReplayBuffer(object):
             np.stack(next_pc_state_batch),
             np.stack(next_joint_state_batch),
             np.stack(reward_batch),
-            np.stack(done_batch)
+            np.stack(done_batch),
+            np.stack(success_batch)
         )
 
     def __len__(self):
         return self.size
 
     def __getitem__(self, idx):
-        pc_state, joint_state, con_action, conti_para, next_pc_state, next_joint_state, reward, done = self.buffer.queue[idx]
+        pc_state, joint_state, con_action, conti_para, next_pc_state, next_joint_state, reward, done, success = self.buffer.queue[idx]
 
         data = {
             "pc_state": np.float32(pc_state),
@@ -62,7 +63,8 @@ class ReplayBuffer(object):
             "next_pc_state": np.float32(next_pc_state),
             "next_joint_state": np.float32(next_joint_state),
             "reward": np.float32(reward),
-            "done": np.float32(done)
+            "done": np.float32(done),
+            "success": np.float(success)
         }
         return data
 
@@ -75,10 +77,10 @@ class ReplayBuffer(object):
     def save_data(self, filename):
         # Convert the contents of the queue to lists
         (pc_state_list, joint_state_list, con_action_list, conti_para_list, next_pc_state_list, next_joint_state_list,
-         reward_list, done_list) = [], [], [], [], [], [], [], []
+         reward_list, done_list, success_list) = [], [], [], [], [], [], [], [], []
 
         for item in self.buffer.queue:
-            pc_state, joint_state, con_action, conti_para, next_pc_state, next_joint_state, reward, done = item
+            pc_state, joint_state, con_action, conti_para, next_pc_state, next_joint_state, reward, done, success = item
             pc_state_list.append(pc_state)
             joint_state_list.append(joint_state)
             con_action_list.append(con_action)
@@ -87,7 +89,7 @@ class ReplayBuffer(object):
             next_joint_state_list.append(next_joint_state)
             reward_list.append(reward)
             done_list.append(done)
-
+            success_list.append(success)
         # Convert lists to NumPy arrays
         pc_state_array = np.array(pc_state_list)
         joint_state_array = np.array(joint_state_list)
@@ -97,6 +99,7 @@ class ReplayBuffer(object):
         next_joint_state_array = np.array(next_joint_state_list)
         reward_array = np.array(reward_list)
         done_array = np.array(done_list)
+        success_array = np.array(success_list)
 
         # Create a dictionary to store the arrays
         data_dict = {
@@ -107,7 +110,8 @@ class ReplayBuffer(object):
             'next_pc_state': next_pc_state_array,
             'next_joint_state': next_joint_state_array,
             'reward': reward_array,
-            'done': done_array
+            'done': done_array,
+            'success': success_array
         }
 
         # Save the data dictionary to a NumPy file
@@ -118,7 +122,7 @@ class ReplayBuffer(object):
         data_dict = np.load(filename, allow_pickle=True)
 
         # Extract individual arrays from the data dictionary
-        arrays = ['pc_state', 'joint_state', 'con_action','conti_para', 'next_pc_state', 'next_joint_state', 'reward', 'done']
+        arrays = ['pc_state', 'joint_state', 'con_action','conti_para', 'next_pc_state', 'next_joint_state', 'reward', 'done', 'success']
         data_list = zip(*(data_dict[array] for array in arrays))
 
         # Clear the current buffer
